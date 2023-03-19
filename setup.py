19730c453a92e9a85d553601363e22d07053e176
Siddharth Saxena <siddharth@appknox.com>
2023-03-20 01:37:18 +0530

    include all data in module

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 	description="ML Based API Key Detector",
 	long_description=description,
 	long_description_content_type="text/markdown",
-	url="https://github.com/alessandrodd/apk_api_key_extractor",
+	url="https://github.com/alessandrodd/api_key_extractor",
 	license='MIT',
 	python_requires='>=3.8',
 	install_requires=REQUIREMENTS
